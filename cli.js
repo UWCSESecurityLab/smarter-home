9e0f92a9ff12ef2bd60536b3c1ca6335938fb86b
Eric Zeng <ericzeng@cs.washington.edu>
2016-12-06 15:12:07 -0800

    Make the whole CLI using co magic

--- cli.js
+++ cli.js
@@ -6,29 +6,29 @@ const opn = require('opn');
 const prompt = require('co-prompt');
 const SmartAppClient = require('./SmartAppClient');
 
-// Start the server, wait for access token in Promise.
-let tokenPromise = authServer.getAccessToken();
+co(function* () {
 
-// Prompt user to log in
-console.log('Please log in to SmartThings in your browser.');
-opn('http://localhost:5000/login');
+  // Start the server, wait for access token in Promise.
+  let tokenPromise = authServer.getAccessToken();
 
-// Once token is retrieved, launch the prompt
-tokenPromise.then(function(accessToken) {
+  // Prompt user to log in
+  console.log('Please log in to SmartThings in your browser.');
+  opn('http://localhost:5000/login');
+
+  // Get the token
+  let accessToken = yield tokenPromise;
   console.log('Access token retrieved:');
   console.log(accessToken);
-
   let client = new SmartAppClient(accessToken.access_token);
 
-  co(function* () {
-    while(true) {
-      var text = yield prompt('$ ');
-      if (text.trim() === 'list') {
-        let endpoints = yield client.listEndpoints();
-        console.log(endpoints);
-      } else {
-        console.log('\'' + text + '\' is not a supported command.');
-      }
+  // This is the main command prompt loop
+  while(true) {
+    var text = yield prompt('$ ');
+    if (text.trim() === 'list') {
+      let endpoints = yield client.listEndpoints();
+      console.log(endpoints);
+    } else {
+      console.log('\'' + text + '\' is not a supported command.');
     }
-  });
+  }
 });
