3056bb759d8daf05a5e56e4ab0495f8b60d3ae2b
Eric Zeng <ericzeng@cs.washington.edu>
2016-12-06 14:51:46 -0800

    Make the server a one-time thing for logins
    Implement server so that it starts when you call the function,
    returns the access token, and closes itself

---
+++ authServer.js
@@ -0,0 +1,64 @@
+'use strict';
+const enableDestroy = require('server-destroy');
+const express = require('express');
+const bodyParser = require('body-parser');
+const queryString = require('query-string');
+const request = require('request');
+
+const client_id = 'e466bc7d-acbd-4489-8970-72d5987e1f30';
+const client_secret = '7b549488-c4a0-4012-8bd8-963d4ea055f3';
+
+exports.getAccessToken = function() {
+  return new Promise((resolve, reject) => {
+    var app = express();
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'hbs');
+    app.use(bodyParser.json());
+    app.use('/static', express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+    app.get('/login', logEndpoint, function(req, res) {
+      let qs = {
+        'response_type': 'code',
+        'client_id': client_id,
+        'scope': 'app',
+        'redirect_uri': 'http://localhost:5000/return/authorize'
+      };
+      res.redirect('https://graph.api.smartthings.com/oauth/authorize?' + queryString.stringify(qs));
+    });
+
+    app.get('/return/authorize', logEndpoint, function(req, res) {
+      request({
+        method: 'POST',
+        url: 'https://graph.api.smartthings.com/oauth/token',
+        headers: {
+          'Host': 'graph.api.smartthings.com',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: queryString.stringify({
+          'grant_type': 'authorization_code',
+          'code': req.query.code,
+          'client_id': client_id,
+          'client_secret': client_secret,
+          'redirect_uri': 'http://localhost:5000/return/authorize'
+        })
+      }, function(err, resp, body) {
+        if (err) {
+          reject(err);
+        }
+        let accessToken = JSON.parse(body);
+        res.send('<html><body>Login successful! You may close this window.</body></html>');
+        server.destroy();
+        resolve(accessToken);
+      });
+    });
+
+    function logEndpoint(req, res, next) {
+      console.log(req.method + ' ' + req.url);
+      next();
+    }
+
+    let server = app.listen(5000);
+    enableDestroy(server);
+    console.log('Auth Server listening on port 5000');
+  });
+}
--- cli.js
+++ cli.js
@@ -1,11 +1,33 @@
 #!/usr/bin/env node
 'use strict';
+const authServer = require('./authServer');
 const co = require('co');
+const opn = require('opn');
 const prompt = require('co-prompt');
+const SmartAppClient = require('./SmartAppClient');
 
-co(function* () {
-  while(true) {
-    var text = yield prompt('$ ');
-    console.log(text);
-  }
-})
+// Start the server, wait for access token in Promise.
+let tokenPromise = authServer.getAccessToken();
+
+// Prompt user to log in
+console.log('Please log in to SmartThings in your browser.');
+opn('http://localhost:5000/login');
+
+// Once token is retrieved, launch the prompt
+tokenPromise.then(function(accessToken) {
+  console.log('Access token retrieved:');
+  console.log(accessToken);
+
+  let client = new SmartAppClient(accessToken.access_token);
+
+  co(function* () {
+    while(true) {
+      var text = yield prompt('$ ');
+      if (text.trim() === 'list') {
+        client.listEndpoints().then(console.log);
+      } else {
+        console.log('text');
+      }
+    }
+  });
+});
