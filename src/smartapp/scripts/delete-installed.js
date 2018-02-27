26bd6a3af15f71601dea0a3647653fd8ec0c81ce
Eric Zeng <ericzeng@cs.washington.edu>
2018-02-27 12:48:35 -0800

    Update logging in delete

--- src/smartapp/scripts/delete-installed.js
+++ src/smartapp/scripts/delete-installed.js
@@ -2,28 +2,40 @@
 // server.
 
 const bearer = require('../config/personal_tokens.json').full_token;
-const InstallData = require('../db/installData');
+const InstallData = require('../server/db/installData');
+const log = require('../server/log');
 const mongoose = require('mongoose');
 const request = require('request');
 
 // The Smart Notifications app.
 const APP_ID = '83972ef3-95f9-4f47-9a58-c305a5f3b565';
 
+const SECLAB_LOC = '1f7b9dce-985b-47a7-9814-ed10f3b71f2f';
+const SHRI_LOC = 'b709e26c-a379-4bc4-8000-53b014a7e7fe';
+
 mongoose.connect('mongodb://localhost/test');
 let db = mongoose.connection;
 db.on('error', console.log.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
+  deleteApps(SECLAB_LOC);
+  // deleteApps(SHRI_LOC);
+});
+
+
+function deleteApps(locationId) {
+  log.blue('SmartThings Request', `GET https://api.smartthings.com/v1/installedapps?locationId=${locationId}`);
   request({
-    url: 'https://api.smartthings.com/v1/installedapps',
+    url: `https://api.smartthings.com/v1/installedapps?locationId=${locationId}`,
     method: 'GET',
     headers: {
       'Authorization': 'Bearer ' + bearer
     }
   }, (err, res, body) => {
     if (err) {
-      console.log(err);
+      log.error(err);
       return;
     }
+    log.log(body);
     let apps = JSON.parse(body);
     Promise.all(apps.items.map((app) => {
       return new Promise((resolve, reject) => {
@@ -31,47 +43,47 @@ db.once('open', function() {
           resolve();
           return;
         }
-
+        log.blue('SmartThings Request', 'DELETE https://api.smartthings.com/v1/installedapps/' + app.installedAppId);
         request({
           url: 'https://api.smartthings.com/v1/installedapps/' + app.installedAppId,
           method: 'DELETE',
           headers: {
             'Authorization': 'Bearer ' + bearer
           }
-        }, (err, res) => {
+        }, (err, res, body) => {
           if (err) {
-            console.log(err);
+            log.error(err);
             reject();
             return;
           }
           if (res.statusCode !== 200) {
-            console.log('Error ' +  res.status);
+            log.error(res.status);
             reject();
             return;
           }
 
-          console.log(app.installedAppId + ' was deleted from SmartThings.');
+          log.log(app.installedAppId + ' was deleted from SmartThings.');
 
           InstallData.findOneAndRemove({
             'installedApp.installedAppId': app.installedAppId
           }, (err) => {
             if (err) {
-              console.log(err);
+              log.error(err);
               reject();
               return;
             }
-            console.log(app.installedAppId + ' was deleted from Mongo.');
+            log.log(app.installedAppId + ' was deleted from Mongo.');
             resolve();
           });
         });
       });
     })).then(() => {
-      console.log('Done.');
+      log.log('Done.');
       process.exit();
     }).catch(() => {
-      console.log('Done, some errors occurred.');
+      log.error('Done, some errors occurred.');
       process.exit();
     });
   });
-});
+}
 
