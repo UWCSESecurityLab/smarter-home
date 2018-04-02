04262f9c5bfbf3e24bc33b908f8bfafeae5b7002
Eric Zeng <ericzeng@cs.washington.edu>
2018-04-02 15:11:05 -0700

    Add multiple options for server origin (still must manually select)

--- src/mobile/origin.js
+++ src/mobile/origin.js
@@ -1,4 +1,11 @@
 // The scheme + hostname + port of the SmartApp server.
-const ORIGIN = 'http://10.0.2.2:5000'
 
-export default ORIGIN;
+// This IP allows the Android emulator to connect to the SmartApp server on the
+// host machine.
+const EMULATOR_ORIGIN = 'http://10.0.2.2:5000'
+
+// This hostname exposes selenium's SmartApp server to other hosts on CSE-Local.
+// Used for physical phone devices.
+const DEVICE_ORIGIN = 'http://selenium.dyn.cs.washington.edu:5000'
+
+export default EMULATOR_ORIGIN;
