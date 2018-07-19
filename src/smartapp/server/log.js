33ef33ac2b21afa7aef48fe75da93e12afab5129
Eric Zeng <ericzeng@cs.washington.edu>
2018-07-19 16:42:42 -0700

    Add timestamp to logs

--- src/smartapp/server/log.js
+++ src/smartapp/server/log.js
@@ -1,4 +1,7 @@
 const colors = require('colors/safe');
+function ts() {
+  return new Date().toLocaleString() + ': ';
+}
 
 function print(type, message) {
   console.log('[' + type + '] ' + message);
@@ -6,39 +9,39 @@ function print(type, message) {
 
 // Basic colors
 exports.black = (type, message) => {
-  print(colors.black(type), message);
+  print(colors.black(ts() + type), message);
 }
 
 exports.red = (type, message) => {
-  print(colors.red(type), message);
+  print(colors.red(ts() + type), message);
 }
 
 exports.green = (type, message) => {
-  print(colors.green(type), message);
+  print(colors.green(ts() + type), message);
 }
 
 exports.yellow = (type, message) => {
-  print(colors.yellow(type), message);
+  print(colors.yellow(ts() + type), message);
 }
 
 exports.blue = (type, message) => {
-  print(colors.blue(type), message);
+  print(colors.blue(ts() + type), message);
 }
 
 exports.magenta = (type, message) => {
-  print(colors.magenta(type), message);
+  print(colors.magenta(ts() + type), message);
 }
 
 exports.cyan = (type, message) => {
-  print(colors.cyan(type), message);
+  print(colors.cyan(ts() + type), message);
 }
 
 exports.white = (type, message) => {
-  print(colors.white(type), message);
+  print(colors.white(ts() + type), message);
 }
 
 exports.gray = (type, message) => {
-  print(colors.gray(type), message);
+  print(colors.gray(ts() + type), message);
 }
 
 // Shorthand functions
