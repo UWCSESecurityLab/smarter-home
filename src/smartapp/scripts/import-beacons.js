e2f5176c1a6b07a93950f1d8fe2d8e8b41dd41d6
Eric Zeng <ericzeng@cs.washington.edu>
2018-08-01 14:43:35 -0700

    Add script for importing beacons from Kontakt.io CSVs

---
+++ src/smartapp/scripts/import-beacons.js
@@ -0,0 +1,44 @@
+const Beacon = require('../server/db/beacon');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const parse = require('csv-parse/lib/sync');
+
+if (process.argv.length !== 3) {
+  console.log('Usage: node import-beacons.js beacon_csv_file');
+  process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+let db = mongoose.connection;
+db.on('error', console.log.bind(console, 'connection error:'));
+db.once('open', () => {
+  let raw = fs.readFileSync(process.argv[2]).toString();
+  let parsed = parse(raw, {columns: true});
+
+  let beacons = parsed.map((b) => {
+    return {
+      namespace: b.namespace,
+      id: b.instanceId,
+      name: b.uniqueId
+    }
+  });
+
+  Beacon.insertMany(beacons, {ordered: false}, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Inserted ${docs.length} beacons`);
+      console.log('Inserted:');
+      console.log(beacons.filter((b) => {
+        return docs.find(d => d.id === b.id);
+      }));
+      console.log('Not inserted:');
+      console.log(beacons.filter((b) => {
+        return !docs.find(d => d.id === b.id);
+      }));
+    }
+    db.close();
+  });
+});
+
+
--- src/smartapp/server/db/beacon.js
+++ src/smartapp/server/db/beacon.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 // This Schema is used to represent individual beacons.
 let beaconSchema = mongoose.Schema({
-  namespace: String, // Namespace of this beacon
-  id: String,        // ID of this beacon.
-  name: String       // User friendly name, physically labeled on beacon
+  namespace: { type: String, required: true },
+  id: { type: String, required: true, unique: true },
+  // User friendly name, physically labeled on beacon
+  name: { type: String, required: true, unique: true }
 });
+beaconSchema.plugin(uniqueValidator);
 
 let Beacon = mongoose.model('Beacon', beaconSchema);
 module.exports = Beacon;
