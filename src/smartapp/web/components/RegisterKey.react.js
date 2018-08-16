2577ec4c388e47e3bda4580d6e6b95255af57b14
Eric Zeng <ericzeng@cs.washington.edu>
2018-08-16 16:22:25 -0700

    [Feature] Join: generate ECDSA key pair and show public key as QR code

--- src/smartapp/web/components/RegisterKey.react.js
+++ src/smartapp/web/components/RegisterKey.react.js
@@ -4,14 +4,38 @@ import Button from '@material/react-button';
 import MaterialIcon from '@material/react-material-icon';
 import { Link, Redirect } from 'react-router-dom';
 
+async function generateDeviceKeys() {
+  console.log('Generating keys...');
+  let keys = await crypto.subtle.generateKey(
+      {name: 'ECDSA', namedCurve: 'P-521'}, true, ['sign', 'verify']);
+
+  let [pub, priv] = await Promise.all([
+    crypto.subtle.exportKey('jwk', keys.publicKey),
+    crypto.subtle.exportKey('jwk', keys.privateKey)
+  ]);
+
+  localStorage.setItem('deviceKeys',
+                       JSON.stringify({publicKey: pub, privateKey: priv}));
+  return pub;
+}
+
 class RegisterKey extends React.Component {
-  componentDidMount() {
-    qrcode.toCanvas(document.getElementById('canvas'), 'public key', (err) => {
+  async componentDidMount() {
+    let publicKey;
+    let deviceKeys = localStorage.getItem('deviceKeys');
+    if (!deviceKeys) {
+      publicKey = await generateDeviceKeys();
+    } else {
+      publicKey = JSON.parse(deviceKeys).publicKey;
+    }
+
+    qrcode.toCanvas(document.getElementById('canvas'), JSON.stringify(publicKey), (err) => {
       if (err) {
         console.error(err);
       }
     });
   }
+
   render() {
     return (
       <div>
