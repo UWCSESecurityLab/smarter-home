ae75e98b0675c90d85cc9c78f7b35e55a77f2106
Eric Zeng <ericzeng@cs.washington.edu>
2018-08-23 19:28:36 -0700

    Revert to HashRouter in CordovaRoot

--- src/smartapp/web/components/CordovaRoot.react.js
+++ src/smartapp/web/components/CordovaRoot.react.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store } from '../redux/reducers';
 import App from './App.react';
